a2d004b4fa6157e25ffd62427b57109eb9d64c8f
naomikwilson <122654335+naomikwilson@users.noreply.github.com>
2023-11-29 15:38:11 -0500

    Update app

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ from historical_events import get_fact
 app = Flask(__name__)
 
 
-@app.post("/page_to_put_it_on")
+@app.post("/homepage")
 def historical_fact_post():
     year, fact = get_fact()
-    return render_template("page_to_put_it_on.html", fact=fact, year=year)
+    return render_template("homepage.html", fact=fact, year=year)
     # on page_to_put_it_on.html, we'd put something like:
     # <p> Historical fact: on this month and day in year {{ year }}, {{ fact }} </p>
 
